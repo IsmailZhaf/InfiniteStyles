4b822a5a866b42f53651d0d31c43c5c9bda04ace
IsmailZhanfeari <ismailzhanfeari@gmail.com>
2024-01-16 01:20:14 +0700

    -

--- src/app/BestSeller/page.js
+++ src/app/BestSeller/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BestSeller } from "@/components/BestSeller/components/BestSeller";
 import { Header } from "@/components/HomePage/Templates/Header";
+import { Footer } from "@/components/HomePage/Templates/Footer";
 
 export default function Page() {
   return (
